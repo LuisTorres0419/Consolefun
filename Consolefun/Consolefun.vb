a149813a55f212522ef0c7bf5de8edcf577ac2be
luis torres <tl224627@gmail.com>
2020-08-24 07:53:04 -0700

    add beep example

--- Consolefun/Consolefun.vb
+++ Consolefun/Consolefun.vb
@@ -1,15 +1,28 @@
-﻿Module Consolefun
+﻿Option Strict Off
+Option Explicit On
+
+
+Module Consolefun
 
     Sub Main()
-        Dim userInput As String
-        Console.WriteLine("Please Type Some Text Then Press Enter...")
-        userInput = Console.ReadLine()
-        Console.WriteLine("Thanks!! You entered: " & userInput & ". Good Work!!")
-        Console.ReadLine()
+        'Dim userInput As String
+        'Console.WriteLine("Please Type Some Text Then Press Enter...")
+        'userInput = Console.ReadLine()
+        'Console.WriteLine("Thanks!! You entered: " & userInput & ". Good Work!!")
+        'Console.ReadLine()
+
+        'Dim firstNumber As Integer
+        'Dim result As Integer
+        'Console.WriteLine("Please Enter A Number")
+        'userInput = Console.ReadLine()
+        'firstNumber = CInt(userInput)
+        ''firstNumber = Console.ReadLine()
+        'result = 5 + firstNumber
+        'Console.WriteLine(result)
+        'Console.ReadLine()
 
 
-
-
+        'Console.Beep()
 
 
     End Sub
